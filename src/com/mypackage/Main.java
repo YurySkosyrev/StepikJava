3d10bc1b46d65d43dc3a495c5f34073eedbecbb4
skosyrev <YurySkosyrev>
2022-04-18 13:20:11 +0300

    Third commit

--- src/com/mypackage/Main.java
+++ src/com/mypackage/Main.java
@@ -1,9 +1,16 @@
 package com.mypackage;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello");
-        // Типы данных
-        //
+        Scanner scan = new Scanner(System.in);
+        short a, b;
+        int c;
+
+        System.out.println("Введите 1 число");
+        a = scan.nextShort();
+        System.out.println("Введите 2 число");
+        b = scan.nextShort();
+        c = a + b;
+        System.out.print(c);
     }
 }
