e7fd146159c5540a2a5e5f8dd5d6c9925ef39c42
skosyrev <skosyrev-87@mail.ru>
2022-05-13 09:57:09 +0300

    Step 4.2.1 done

--- src/org/Stepik/Task_4_2_1/Task.java
+++ src/org/Stepik/Task_4_2_1/Task.java
@@ -1,19 +1,36 @@
 package org.Stepik.Task_4_2_1;
 
 public class Task {
-    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
-        int i = 3;
-        while (i > 0){
-            try{
-                RobotConnection rc = robotConnectionManager.getConnection();
+    // решение с ресурсами
+    public static void moveRobot1 (RobotConnectionManager robotConnectionManager, int toX, int toY) {
+        for(int i = 0; i < 3; i++){
+            try(RobotConnection rc = robotConnectionManager.getConnection()){
                 rc.moveRobotTo(toX, toY);
-                rc.close();
-                i = 0;
+                i = 3;
             }
             catch(RobotConnectionException e){
-                if (i == 1) throw new  RobotConnectionException("error");
-                i-- ;
+                if (i == 2) throw new  RobotConnectionException("error");
             }
         }
-
     }
+
+    // решение без ресурсов
+    public static void moveRobot2(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+        for(int i = 0; i < 3; i++){
+            try{
+                RobotConnection rc = robotConnectionManager.getConnection();
+                try {
+                    rc.moveRobotTo(toX, toY);
+                    i = 3;
+                }
+                finally{
+                    rc.close();
+                }
+            }
+            catch(RobotConnectionException e){
+                if (i == 2) throw new  RobotConnectionException("error");
+            }
+        }
+    }
+
+}
