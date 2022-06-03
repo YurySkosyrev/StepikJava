12cdce51b79bd5c27825ba024491849ab8223cfc
skosyrev <skosyrev-87@mail.ru>
2022-06-03 16:44:02 +0300

    MineSweeper

--- src/org/Stepik/Sweeper/Main.java
+++ src/org/Stepik/Sweeper/Main.java
@@ -8,12 +8,56 @@ public class Main {
                 Scanner sc = new Scanner(System.in);
                 int x = sc.nextInt();
                 int y = sc.nextInt();
+                int mineX, mineY;
+                int [][] field = new int[x][y];
                 int mines = sc.nextInt();
-                int [][] coord = new int[mines][];
-                for (int i =0; i < 3; i++)
+                int [][] coord = new int[mines][2];
+                for (int i =0; i < mines; i++)
                 {
-                    coord[i][0] = sc.nextInt();
-                    coord[i][1] = sc.nextInt();
+                    coord[i][0] = sc.nextInt()-1;
+                    coord[i][1] = sc.nextInt()-1;
                 }
+
+                for (int i =0; i < mines; i++)
+                {
+                     mineX = coord[i][0];
+                     mineY = coord[i][1];
+                     field[mineX][mineY]=-1;
+                }
+
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        if (field[i][j] == -1) {
+                            for (int k = i - 1; k <= i + 1; k++){
+                                for (int l = j - 1; l <= j + 1; l++){
+                                    if (check(k, l, x, y)){
+                                        if (field[l][k] != -1) field[l][k] ++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                if (field[i][j] == -1) {
+                    System.out.print("m ");
+                }else{
+                    System.out.print(field[i][j] + " ");
+                }
+            }
+            System.out.println();
         }
+
+
+
+        }
+    static boolean check(int i, int j, int n, int m){
+        return i >= 0 && i < n && j >= 0 && j < m;
+    }
 }
